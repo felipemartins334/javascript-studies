b0e7e0efb21ec9dba58e2a27b254107c03467e37
Felipe Martins <felipemartins0304@hotmail.com>
2022-02-09 10:43:48 -0300

    Iterators done

--- iterators/iterators.js
+++ iterators/iterators.js
@@ -1,4 +1,51 @@
-const numbers = [1,2,3];
+const usersByCities = {
+  ['Buenos Aires']: [
+    'Javier',
+    'Charles',
+    'Bill'
+  ],
+  ['Salt Lake City']: [
+    'Morgan',
+    'Marston',
+    'Dutch'
+  ],
+  ['Paris']: [
+    'Jean Valjean',
+    'Cosette',
+    'Fantine'
+  ],
 
-const numbersIterator = numbers[Symbol.iterator]()
-console.log(numbersIterator)
+  [Symbol.iterator]: function(){
+
+    const cityKeys = Object.keys(this)
+    let userIndex = 0
+    let cityIndex = 0
+
+    return {
+
+      next: () => {
+        if(cityIndex > cityKeys.length - 1){
+          return {
+            done: true,
+            value: undefined
+          }
+        }
+        let city = this[cityKeys[cityIndex]]
+        let user = city[userIndex]
+        userIndex++
+        if(userIndex > city.length - 1){
+          cityIndex ++
+          userIndex=0
+        }
+        return {
+          done: false,
+          value: user
+        }
+      }
+    }
+  }
+}
+
+for(let user of usersByCities){
+  console.log(user)
+}
